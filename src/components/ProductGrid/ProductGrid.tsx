da714aabf2e1ecd705ffb7741b0ea556ebfa4945
Diganta-18 <digantab.cse.jisu21@gmail.com>
2024-04-10 04:10:55 +0530

    Error resolved

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
 const ProductGrid = () => {
-  return (
-    <div>
-      ProductGrid.tsx
-    </div>
-  )
-}
+  return <div>ProductGrid.tsx</div>;
+};
 
-export default ProductGrid
+export default ProductGrid;
